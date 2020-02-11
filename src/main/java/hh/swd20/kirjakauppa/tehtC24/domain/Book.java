bd6a2b60721c51ebbf0baf91e79430678a270728
Henri Vaisanen <henri.vaisane@gmail.com>
2020-02-11 14:17:58 +0200

    Book luokkaan lisätty getterit ja setterit

--- src/main/java/hh/swd20/kirjakauppa/tehtC24/domain/Book.java
+++ src/main/java/hh/swd20/kirjakauppa/tehtC24/domain/Book.java
@@ -15,4 +15,43 @@ public class Book {
 
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
